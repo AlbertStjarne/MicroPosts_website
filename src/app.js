8224b67190079c44c57660ea5b079fb9101ac08f
Albert Stjärne <albert.stjarne@gmail.com>
2019-03-08 15:40:26 +0100

    enable edit by opening the post when clicking edit icon

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Listen for delete
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
 // Get posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -39,16 +42,38 @@ function submitPost() {
 
 // Delete post
 function deletePost(e) {
-  e.preventDefault();
   if(e.target.parentElement.classList.contains('delete')) {
     const id = e.target.parentElement.dataset.id;
     if(confirm('Are you sure you want to delete the post?')) {
       http.delete(`http://localhost:3000/posts/${id}`)
-        .then(data => {
-          ui.showAlert('Post removed', 'alert alert-success');
-          getPosts();
-        })
-        .catch(err => console.log(err));
+      .then(data => {
+        ui.showAlert('Post removed', 'alert alert-success');
+        getPosts();
+      })
+      .catch(err => console.log(err));
     }
   }
+
+  e.preventDefault();
+}
+
+// Enable edit state
+function enableEdit(e) {
+  console.log(e.target);
+  if(e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+    
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill form with current post
+    ui.fillForm(data);
+  }
+
+  e.preventDefault();
 }
